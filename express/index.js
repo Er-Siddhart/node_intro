ae4f49df0f351306b89ed3588a1b072129c602c4
sid <er.siddhart@gmail.com>
2024-05-31 19:10:34 +0530

    forth  commit

--- express/index.js
+++ express/index.js
@@ -19,7 +19,7 @@ const app=express();
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(adminRoutes);
-app.use(shopRoutes);
+app.use('/shop',shopRoutes);
 
 app.use((req,res,next)=>{
     res.status(404).send('<h1>Page not found</h1>')
--- express/routes/admin.js
+++ express/routes/admin.js
@@ -2,12 +2,12 @@ const express =require('express');
 
 const router=express.Router();
 
-router.use('/add-product',(req,res,next)=>{
+router.use('admin/add-product',(req,res,next)=>{//making a seperate filter 
     // console.log('add-product');
     res.send('<form action="/product" method="POST"><input type="text" name="content"><button type="submit">Add content</button> </form>')
 })
 
-router.use('/product',(req,res,next)=>{
+router.use('admin/product',(req,res,next)=>{
     console.log(req.body);
     res.send('<h1>submitted</h1>')
 })
